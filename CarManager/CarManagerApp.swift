1b2f3fca6ab436e17f378d5b9e890300387dab78
Kirill Kubarskiy <45077920+kubarlk@users.noreply.github.com>
2023-04-15 18:33:48 +0300

    Initial Commit

---
+++ CarManager/CarManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarManagerApp.swift
+//  CarManager
+//
+//  Created by Kirill Kubarskiy on 15.04.23.
+//
+
+import SwiftUI
+
+@main
+struct CarManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
